aae6372e057459d11223eaf790f0199e36742006
KillMonkey <1249082798@qq.com>
2018-06-09 11:42:37 +0800

    spingboot代码初始化

---
+++ ElasticSearchDemo/src/main/java/com/xiaoge/ElasticSearchDemo/controller/DemoController.java
@@ -0,0 +1,15 @@
+package com.xiaoge.ElasticSearchDemo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    
+    @RequestMapping("/hello")
+    public String hello()
+    {
+        return "Hello World!";
+    }
+
+}
